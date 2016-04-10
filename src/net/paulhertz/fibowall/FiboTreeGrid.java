28cb712f0b8977a6413e57628bd13f90839aec03
Paul Hertz <ignotus@gmail.com>
2016-04-10 15:36:16 -0500

    Revised and refactored FiboGridTree
    Settled on 144 grid spaces, output ruled lines.

--- src/net/paulhertz/fibowall/FiboTreeGrid.java
+++ src/net/paulhertz/fibowall/FiboTreeGrid.java
@@ -15,20 +15,24 @@ public class FiboTreeGrid extends PApplet {
 	UniBlox bloxx = new UniBlox();
 	String seedString = "";
 	String outString = "";
+	StringBuffer gridBuf;
 	int depth = 5;
 	public ArrayList<BezShape> shapes;
+	public ArrayList<BezShape> grid;
 	IgnoCodeLib igno;
+	float tw = 1;
+	float gridX = 0;
+	float gridY = 0;
 
 	public static final float GOLDEN = (float)((Math.sqrt(5) - 1)/2.0 + 1);
 	public static final float INVGOLDEN = GOLDEN - 1;
 	public static final float NEXTGOLDEN = GOLDEN + 1;
 	float w = GOLDEN;
-	float tw = 1;
 	int[] FIB = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 
 							  2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 
 							  317811, 514229, 832040, 1346269 };
 
-	String filePath = "/Users/paulhz/Desktop/Eclipse_output/fibotree";
+	String filePath = "/Users/paulhz/Desktop/Eclipse_output/fibotree/grid";
 	String basename = "";
 	int fileCount = 1;
 
@@ -40,13 +44,18 @@ public class FiboTreeGrid extends PApplet {
 		noLoop();
 		igno = new IgnoCodeLib(this);
 		sb = new StringBuffer(1024);
+		gridBuf = new StringBuffer();
 		shapes = new ArrayList<BezShape>();
+		grid = new ArrayList<BezShape>();
 		// depth of 17 yields 1597 bands
-		depth = 11;
+		// depth of 11 yields 144 bands, FIB[depth + 1] == 144;
+		// 11 is ideal for the grid: it divides each of the 16 panels into 9 equal parts
+		depth = 12;
 		initLindenmeyer_000();
 		expandString(seedString, depth);
 		decodeString();
 		loadShapes();
+		println(gridBuf.toString());
 	}
 
 	public static void main(String args[]) {
@@ -56,6 +65,9 @@ public class FiboTreeGrid extends PApplet {
 	public void draw() {
 		background(255);
 		for (BezShape bez: shapes) {
+			bez.draw();
+		}
+		for (BezShape bez: grid) {
 			bez.draw();
 		}
 	}
@@ -77,19 +89,36 @@ public class FiboTreeGrid extends PApplet {
 
 	public void expandString(String tokens, int level) {
 		// println("level is "+ level + "\n  "+ tokens);
+		// construct the string representation of the grid line by line
 		int pos = level + 1;
-		int fib1 = FIB[pos + 1] - 1;
-		int fib0 = FIB[pos] - 1;
-		String pad1 = pad('_', fib1);
-		String pad0 = pad(' ', fib0);
-		StringBuffer gridBuf = new StringBuffer();
+		int fib0 = FIB[pos];
+		int fib1 = FIB[pos + 1];
+		String pad0 = pad(' ', fib0 - 1);
+		String pad1 = pad('_', fib1 - 1);
+		gridX = 0;
+		float yStep = height/(depth + 1.0f);
+		float xStep = ((float) width)/(FIB[depth + 1]);
+		noFill();
+		stroke(128);
 		for (int i = 0; i < tokens.length(); i++) {
 			char ch = tokens.charAt(i);
-			if ('0' == ch) gridBuf.append(ch + pad0);
-			else if ('1' == ch) gridBuf.append(ch + pad1);
+			if ('0' == ch) {
+				gridBuf.append(ch + pad0);
+				// grid.add(BezRectangle.makeLeftTopWidthHeight(gridX, gridY, fib0 * xStep, height/(depth + 1)));
+				grid.add(BezLine.makeCoordinates(gridX, gridY, gridX, gridY + yStep));
+				gridX += fib0 * xStep;
+			}
+			else if ('1' == ch) {
+				gridBuf.append(ch + pad1);
+				// grid.add(BezRectangle.makeLeftTopWidthHeight(gridX, gridY, fib1 * xStep, height/(depth + 1)));
+				grid.add(BezLine.makeCoordinates(gridX, gridY, gridX, gridY + yStep));
+				gridX += fib1 * xStep;
+			}
 			else println("error: ch = "+ ch);
 		}
-		println(gridBuf.toString());
+		gridY += yStep;
+		gridBuf.append(RETURN);
+		// now do the expansion
 		StringBuffer temp = new StringBuffer(2 * tokens.length());
 		for (int i = 0; i < tokens.length(); i++) {
 			char ch = tokens.charAt(i);
@@ -231,17 +260,28 @@ public class FiboTreeGrid extends PApplet {
 		for (BezShape b : comps) {
 			comp.add(b);
 		}
-		// create some guides (finish making them into guides in Illustrator)
+		// add the grid layer
+		comp = new LayerComponent(this, "Grid", 2);
+		doc.add(comp);
+		for (BezShape b : grid) {
+			comp.add(b);
+		}
+		// create some panel guides (finish making them into guides in Illustrator)
 		int panelCount = 16;
 		int panelWidth = width/panelCount;
-		comp = new LayerComponent(this, "Guidelines", 2);
+		comp = new LayerComponent(this, "Panels", 3);
 		comp.hide();
 		doc.add(comp);
+		noFill();
+		stroke(127);
+		strokeWeight(1);
 		for (int i = 1; i < panelCount; i++) {
 			BezLine bzline = BezLine.makeCoordinates(i * panelWidth, 0, i * panelWidth, height);
 			comp.add(bzline);
 		}
-		fill(255,255,255,192);
+		// add the portal as a transparent rectangle in the Panels layer
+		fill(192,192,192,127);
+		noStroke();
 		BezRectangle bzrect = BezRectangle.makeLeftTopRightBottom(10 * panelWidth, 0, 13 * panelWidth, height);
 		comp.add(bzrect);
 		doc.write(output);
